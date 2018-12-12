76aa026a425c3e75e9f52d055fe3511f45327a10
Lance <ldmcguir13+codecademy@gmail.com>
2018-12-11 16:14:53 -0800

    Steps 29-35

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -8,8 +8,8 @@ class Playlist extends React.Component {
         return (
             <div className="Playlist">
                 <input defaultValue={'New Playlist'}/>
-                <TrackList></TrackList>
-                <a className="Playlist-save">SAVE TO SPOTIFY</a>
+                <TrackList />
+                <button className="Playlist-save">SAVE TO SPOTIFY</button>
             </div>
         )
     }
--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -4,10 +4,12 @@ import './Track.css';
 class Track extends React.Component {
     renderAction () {
         if(this.props.isRemoval) {
-            return (<a className="Track-action" onClick={this.removeTrack}>-</a>);
+            // eslint-disable-next-line
+            return (<button className="Track-action" onClick={this.removeTrack}>-</button>);
         }
         else {
-            return (<a className="Track-action" onClick={this.addTrack} >+</a>);
+            // eslint-disable-next-line
+            return (<button className="Track-action" onClick={this.addTrack} >+</button>);
         }
 
     }
@@ -16,8 +18,8 @@ class Track extends React.Component {
         return (
             <div className="Track">
                 <div className="Track-information">
-                    <h3><!-- track name will go here --></h3>
-                    <p><!-- track artist will go here--> | <!-- track album will go here --></p>
+                    <h3>{this.props.track.name} </h3>
+                    <p>{this.props.track.artist} | {this.props.track.album}</p>
                 </div>
                 {this.renderAction()}
             </div>
